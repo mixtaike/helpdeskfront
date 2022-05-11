9404fe0e31cbe70acf2df06e044d95fa12fd5dcc
Mixtaike <mixtaike@gmail.com>
2022-05-11 12:29:43 -0300

    Criação de Model TECNICOS para modelar a tabela de acordo com os tecnico(tabela HTML e TYPESCRIPT

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Tecnico } from './../../../models/tecnico';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] =[
+    {id:1,
+    nome:'valdir',
+    cpf : '123.567.889-10',
+    email: 'ola@gmail.com',
+    senha:'1234',
+    perfis:['0'],
+    dataCriacao: '15/02/2020'
+  }
+  ]
+
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes' ];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
