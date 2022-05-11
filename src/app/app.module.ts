4b9f589aa40494924c11b0c3c833071a53739be7
Mixtaike <mixtaike@gmail.com>
2022-05-11 11:12:37 -0300

    Componente Header criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AngularMaterialModule } from './sharedModule/angular-material/angular-m
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
