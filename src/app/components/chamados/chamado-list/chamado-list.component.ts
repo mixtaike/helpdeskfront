a211bfd7a46e8da645264f0c5fc056e9ed9e96c8
Mixtaike <mixtaike@gmail.com>
2022-05-12 22:06:53 -0300

    Filtro e metodo findAll para Chamado

--- src/app/components/chamados/chamado-list/chamado-list.component.ts
+++ src/app/components/chamados/chamado-list/chamado-list.component.ts
@@ -1,3 +1,4 @@
+import { ChamadoService } from './../../../services/chamado.service';
 import { Chamado } from './../../../models/chamado';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
@@ -11,33 +12,66 @@ import { MatTableDataSource } from '@angular/material/table';
 export class ChamadoListComponent implements OnInit {
 
 
-  ELEMENT_DATA: Chamado[] =[
-    {
-      id: 1,
-      dataAbertura:'21/06/2021',
-      dataFechamento: '21/06/2021',
-      prioridade: 'ALTA',
-      status: 'ANDAMENTO',
-      titulo: 'Chamado 1',
-      descricao: 'Teste chamado 1',
-      tecnico: 1,
-      cliente:6,
-      nomeCliente: 'Valdir Cezar',
-      nomeTecnico: 'Albert Einstein'
-    }
-  ]
+  ELEMENT_DATA: Chamado[] =[ ]
+  FILTERED_DATA: Chamado[] =[ ]
 
 
   displayedColumns: string[] = ['id', 'titulo', 'nomeTecnico', 'nomeCliente', 'dataAbertura','prioridade','status','acoes' ];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(private service: ChamadoService) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
+
+  findAll(): void {
+    this.service.findAll().subscribe(resposta=> {
+      this.ELEMENT_DATA =resposta;
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  retornaStatus(status : any): string{
+    if (status == '0') {
+      return 'ABERTO'
+    } else if (status == '1'){
+      return 'EM ENDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade : any): string{
+    if (prioridade == '0') {
+      return 'BAIXA'
+    } else if (prioridade == '0'){
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+
+
+  }
+
+  orderByStatus(status: any): void{
+      let list: Chamado[] = [];
+      this.ELEMENT_DATA.forEach(element => {
+        if(element.status == status) {
+          list.push(element)
+        }
+
+      });
+      this.FILTERED_DATA =list;
+      this.dataSource = new MatTableDataSource<Chamado>(list);
+      this.dataSource.paginator = this.paginator;
+  }
+
 }
